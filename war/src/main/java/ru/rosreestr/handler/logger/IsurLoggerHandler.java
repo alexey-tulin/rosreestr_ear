4fa2732b3d11050ede5289b456f8a8ce547811ce
KatrinaBosh <katrinabosh@gmail.ru>
2016-10-17 00:34:54 +0400

    handler to service

--- war/src/main/java/ru/rosreestr/handler/logger/IsurLoggerHandler.java
+++ war/src/main/java/ru/rosreestr/handler/logger/IsurLoggerHandler.java
@@ -1,6 +1,5 @@
 package ru.rosreestr.handler.logger;
 
-import org.springframework.stereotype.Component;
 import ru.rosreestr.handler.LoggerHandler;
 import ru.rosreestr.persistence.model.WebServiceCode;
 
@@ -9,7 +8,6 @@ import ru.rosreestr.persistence.model.WebServiceCode;
  * Назначение: логирование входящих/исходящих сообщений.
  * Принадлежит сервису с кодом ISUR.
  */
-@Component
 public class IsurLoggerHandler extends LoggerHandler {
 
     public static final WebServiceCode CODE = WebServiceCode.ISUR;
